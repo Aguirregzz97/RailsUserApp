d06c83ab5bad5f526ff4272aefb45b15c39f9ffd
Andres Aguirre <aguirregzz97@gmail.com>
2019-06-03 11:08:09 -0500

    login working!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+
+  get 'sessions/new'
+
   get 'users/new'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get    '/signup',  to: 'users#new'
 
   get 'static_pages/home'
   resources :users
